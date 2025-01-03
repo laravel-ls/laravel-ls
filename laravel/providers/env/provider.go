d7315032fd7fe7bb4d806e811b467fa5bb68cb11
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-03 15:18:25 +0100

    laravel/providers/env/provider.go: do not populate the repositor on init. do it before querying the repo.

--- laravel/providers/env/provider.go
+++ laravel/providers/env/provider.go
@@ -3,10 +3,13 @@ package env
 import (
 	"path"
 
+	"laravel-ls/cache"
 	"laravel-ls/file"
 	"laravel-ls/laravel/providers/env/queries"
 	"laravel-ls/lsp/protocol"
 	"laravel-ls/provider"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type Provider struct {
@@ -24,23 +27,33 @@ func (p *Provider) Register(manager *provider.Manager) {
 
 func (p *Provider) Init(ctx provider.InitContext) {
 	p.rootPath = ctx.RootPath
+}
 
+func (p *Provider) updateRepo(logger *log.Entry, FileCache *cache.FileCache) bool {
 	filePath := path.Join(p.rootPath, ".env")
 
-	envFile, err := ctx.FileCache.Open(filePath)
+	envFile, err := FileCache.Open(filePath)
 	if err != nil {
-		ctx.Logger.WithError(err).Error("failed to open env file")
+		logger.WithError(err).Error("failed to open env file")
+		return false
 	}
 
 	if err := p.repo.Load(envFile); err != nil {
-		ctx.Logger.WithError(err).Error("failed to parse env file")
+		logger.WithError(err).Error("failed to parse env file")
+		return false
 	}
+
+	return true
 }
 
 func (p *Provider) Hover(ctx provider.HoverContext) {
 	node := queries.EnvCallAtPosition(ctx.File, ctx.Position)
 
 	if node != nil {
+		if !p.updateRepo(ctx.Logger, ctx.FileCache) {
+			return
+		}
+
 		key := queries.GetKey(node, ctx.File.Src)
 		if len(key) < 1 {
 			return
@@ -65,6 +78,10 @@ func (p *Provider) Hover(ctx provider.HoverContext) {
 func (p *Provider) ResolveDefinition(ctx provider.DefinitionContext) {
 	node := queries.EnvCallAtPosition(ctx.File, ctx.Position)
 	if node != nil {
+		if !p.updateRepo(ctx.Logger, ctx.FileCache) {
+			return
+		}
+
 		key := queries.GetKey(node, ctx.File.Src)
 		if meta, found := p.repo.Get(key); found {
 			ctx.Publish(protocol.Location{
@@ -84,6 +101,10 @@ func (p *Provider) ResolveCompletion(ctx provider.CompletionContext) {
 	node := queries.EnvCallAtPosition(ctx.File, ctx.Position)
 
 	if node != nil {
+		if !p.updateRepo(ctx.Logger, ctx.FileCache) {
+			return
+		}
+
 		text := queries.GetKey(node, ctx.File.Src)
 
 		for key, meta := range p.repo.Find(text) {
@@ -98,16 +119,24 @@ func (p *Provider) ResolveCompletion(ctx provider.CompletionContext) {
 
 func (p *Provider) Diagnostic(ctx provider.DiagnosticContext) {
 	// Find all env calls in the file.
-	for _, capture := range queries.EnvCalls(ctx.File) {
-		key := queries.GetKey(&capture.Node, ctx.File.Src)
+	captures := queries.EnvCalls(ctx.File)
 
-		// Report diagnostic if key is not defined.
-		if !p.repo.Exists(key) {
-			ctx.Publish(provider.Diagnostic{
-				Range:    capture.Node.Range(),
-				Severity: protocol.SeverityError,
-				Message:  "Environment variable is not defined",
-			})
+	if len(captures) > 0 {
+		if !p.updateRepo(ctx.Logger, ctx.FileCache) {
+			return
+		}
+
+		for _, capture := range captures {
+			key := queries.GetKey(&capture.Node, ctx.File.Src)
+
+			// Report diagnostic if key is not defined.
+			if !p.repo.Exists(key) {
+				ctx.Publish(provider.Diagnostic{
+					Range:    capture.Node.Range(),
+					Severity: protocol.SeverityError,
+					Message:  "Environment variable is not defined",
+				})
+			}
 		}
 	}
 }
