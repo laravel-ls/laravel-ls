b6bb16e10e66a64e30324e9df3d9cce43272c86a
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-05 18:31:48 +0100

    Adding file/type_test.go

---
+++ file/type_test.go
@@ -0,0 +1,50 @@
+package file_test
+
+import (
+	"testing"
+
+	"laravel-ls/file"
+)
+
+func Test_TypeByFilename(t *testing.T) {
+	tests := []struct {
+		name     string
+		filename string
+		expected file.Type
+	}{
+		{
+			name:     "Blade",
+			filename: "/path/to/file.blade.php",
+			expected: file.TypeBlade,
+		},
+		{
+			name:     "PHP",
+			filename: "/path/to/file.php",
+			expected: file.TypePHP,
+		},
+		{
+			name:     "Env",
+			filename: "/path/to/.env",
+			expected: file.TypeEnv,
+		},
+		{
+			name:     "Env Example",
+			filename: "/path/to/.env.example",
+			expected: file.TypeEnv,
+		},
+		{
+			name:     "Env local",
+			filename: "/path/to/.env.local",
+			expected: file.TypeEnv,
+		},
+	}
+
+	for _, args := range tests {
+		t.Run(args.name, func(t *testing.T) {
+			actual := file.TypeByFilename(args.filename)
+			if actual != args.expected {
+				t.Errorf("%v is not equal to %v", actual, args.expected)
+			}
+		})
+	}
+}
