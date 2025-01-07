1a5c45247e0111d6517fb32bb46ac83e01832ea0
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-07 10:57:16 +0100

    parser/capture.go: add CaptureSlice type and In() and At() methods

--- parser/capture.go
+++ parser/capture.go
@@ -1,10 +1,34 @@
 package parser
 
 import (
+	"github.com/laravel-ls/laravel-ls/treesitter"
 	ts "github.com/tree-sitter/go-tree-sitter"
 )
 
 type Capture struct {
 	Name string
 	Node ts.Node
+}
+
+type CaptureSlice []Capture
+
+// Find a capture at a given position
+func (captures CaptureSlice) At(position ts.Point) *ts.Node {
+	for _, capture := range captures {
+		if treesitter.PointInRange(position, capture.Node.Range()) {
+			return &capture.Node
+		}
+	}
+	return nil
+}
+
+// Find all captures in a given range
+func (captures CaptureSlice) In(r ts.Range) []*ts.Node {
+	nodes := []*ts.Node{}
+	for _, capture := range captures {
+		if treesitter.RangeOverlap(r, capture.Node.Range()) {
+			nodes = append(nodes, &capture.Node)
+		}
+	}
+	return nodes
 }
