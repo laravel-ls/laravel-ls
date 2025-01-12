abba963520a4cdb1537f5b2186d4c9dc7d07e2b2
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-12 10:29:28 +0100

    treesitter/queries/query.go: add GetQuery() function.

--- treesitter/queries/query.go
+++ treesitter/queries/query.go
@@ -12,12 +12,16 @@ var query embed.FS
 
 var ErrQueryNotFound error = errors.New("query not found")
 
-func GetInjectionQuery(lang string) (string, error) {
-	q, err := query.ReadFile(path.Join(lang, "injections.scm"))
+func GetQuery(lang, name string) (string, error) {
+	q, err := query.ReadFile(path.Join(lang, name+".scm"))
 
 	// if file does not exist, return ErrQueryNotFound.
 	if err != nil && errors.Unwrap(err) == fs.ErrNotExist {
 		err = ErrQueryNotFound
 	}
 	return string(q), err
+}
+
+func GetInjectionQuery(lang string) (string, error) {
+	return GetQuery(lang, "injections")
 }
