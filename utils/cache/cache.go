013c2ea883f8f397920ea8d26de1ebd58127e12c
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-14 23:56:12 +0100

    adding utils/cache package

---
+++ utils/cache/cache.go
@@ -0,0 +1,54 @@
+package cache
+
+// Cache is a simple generic datatype that caches key-value pairs.
+type Cache[T comparable] struct {
+	items map[string]T
+}
+
+// Create a new cache
+func New[T comparable]() *Cache[T] {
+	return &Cache[T]{
+		items: map[string]T{},
+	}
+}
+
+// Get a value from the cache by its key.
+// It returns the value and a boolean indicating
+// if the key was found in the cache.
+func (c Cache[T]) Get(key string) (v T, hit bool) {
+	v, hit = c.items[key]
+	return
+}
+
+// Set a value in the cache.
+// If the key already exists, its value is updated.
+func (c *Cache[T]) Set(key string, value T) {
+	c.items[key] = value
+}
+
+// Gets a value from the cache or generate the value using the provided
+// callback if the key is not found. The callback function takes the
+// key as input and returns a value and an error. If the callback
+// succeeds (non-nil error), the value is cached and returned.
+func (c *Cache[T]) Remember(key string, callback func(key string) (T, error)) (T, error) {
+	if value, hit := c.Get(key); hit {
+		return value, nil
+	}
+
+	value, err := callback(key)
+	if err == nil {
+		c.Set(key, value)
+	}
+	return value, err
+}
+
+// Items returns the entire map of cached items.
+func (c Cache[T]) Items() map[string]T {
+	return c.items
+}
+
+// Clear removes all items from the cache,
+// resetting it to an empty state.
+func (c *Cache[T]) Clear() {
+	c.items = map[string]T{}
+}
---
+++ utils/cache/cache_test.go
@@ -0,0 +1,80 @@
+package cache_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/laravel-ls/laravel-ls/utils/cache"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCache_GetSet(t *testing.T) {
+	c := cache.New[int]()
+	c.Set("one", 1)
+	c.Set("two", 2)
+
+	v, ok := c.Get("one")
+	require.Equal(t, 1, v)
+	require.True(t, ok)
+
+	v, ok = c.Get("three")
+	require.Equal(t, 0, v)
+	require.False(t, ok)
+}
+
+func TestCache_Remember(t *testing.T) {
+	c := cache.New[int]()
+
+	v, err := c.Remember("key", func(key string) (int, error) {
+		require.Equal(t, key, "key")
+		return 123, nil
+	})
+
+	require.Equal(t, v, 123)
+	require.NoError(t, err)
+
+	// Check that the value is stored in the cache
+	v, ok := c.Get("key")
+
+	require.Equal(t, v, 123)
+	require.True(t, ok)
+
+	// Test when error is returned
+	v, err = c.Remember("key2", func(key string) (int, error) {
+		require.Equal(t, key, "key2")
+		return 332, errors.New("test error")
+	})
+
+	require.Equal(t, v, 332)
+	require.Error(t, err)
+
+	// Check that the value is not stored in cache.
+	v, ok = c.Get("key2")
+
+	require.Equal(t, v, 0)
+	require.False(t, ok)
+}
+
+func TestCache_Items(t *testing.T) {
+	c := cache.New[string]()
+
+	c.Set("one", "my_string")
+	c.Set("two", "my_other_string")
+
+	expected := map[string]string{
+		"one": "my_string",
+		"two": "my_other_string",
+	}
+
+	require.Equal(t, expected, c.Items())
+}
+
+func TestCache_Clear(t *testing.T) {
+	c := cache.New[complex64]()
+
+	c.Set("compl1", 0.123+1.234i)
+	c.Set("compl2", 0.9271+32.8121i)
+	c.Clear()
+
+	require.Equal(t, 0, len(c.Items()))
+}
