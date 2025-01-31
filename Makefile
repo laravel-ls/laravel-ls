97c12814fd2bea9f2fdaf84f1072f5a3ace0c73a
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-01-31 06:24:30 +0100

    cmd/laravel-ls/main.go: add version variable that forwards it to program.VersionOverride
    makes the linking flag shorter.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 GO = go
 VERSION=$(shell git describe --always --tags --dirty --match="v*")
-GOLDFLAGS=-s -w -X github.com/laravel-ls/laravel-ls/program.VersionOverride="$(VERSION)"
+GOLDFLAGS=-s -w -X main.version="$(VERSION)"
 GOBUILDFLAGS+=-v -p $(shell nproc) -ldflags="$(GOLDFLAGS)"
 
 .PHONY: build
--- cmd/laravel-ls/main.go
+++ cmd/laravel-ls/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/laravel-ls/laravel-ls/cmd/laravel-ls/cmd"
+	"github.com/laravel-ls/laravel-ls/program"
 	log "github.com/sirupsen/logrus"
 )
 
+var version string = ""
+
 func main() {
+	program.VersionOverride = version
+
 	if err := cmd.Run(); err != nil {
 		log.WithError(err).Error("Error")
 	}
