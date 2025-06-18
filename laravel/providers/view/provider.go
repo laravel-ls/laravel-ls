217d368ee67244c1711f40ebe523e585e44ff7aa
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-06-18 22:10:09 +0200

    laravel/providers/view/provider.go: use new view finder implementation

--- laravel/providers/view/provider.go
+++ laravel/providers/view/provider.go
@@ -1,33 +1,34 @@
 package view
 
 import (
+	"fmt"
 	"path"
+	"path/filepath"
 
 	"github.com/laravel-ls/laravel-ls/file"
-	"github.com/laravel-ls/laravel-ls/laravel"
 	"github.com/laravel-ls/laravel-ls/laravel/providers/view/queries"
+	"github.com/laravel-ls/laravel-ls/laravel/view"
 	"github.com/laravel-ls/laravel-ls/lsp/protocol"
 	"github.com/laravel-ls/laravel-ls/provider"
 	"github.com/laravel-ls/laravel-ls/treesitter/php"
+	"github.com/spf13/afero"
 )
 
 type Provider struct {
 	rootPath string
-	fs       filesystem
+	finder   *view.Finder
 }
 
 func NewProvider() *Provider {
 	return &Provider{
-		fs: filesystem{
-			// TODO: should get the actual directories
-			// from current laravel configuration.
-			paths: []string{"resources/views"},
-		},
+		finder: view.NewFinder(afero.NewOsFs()),
 	}
 }
 
 func (p *Provider) Init(ctx provider.InitContext) {
 	p.rootPath = ctx.RootPath
+	p.finder.AddLocation(path.Join(p.rootPath, "resources/views"))
+	p.finder.RegisterExtensions(".blade.php")
 }
 
 func (p *Provider) Register(manager *provider.Manager) {
@@ -46,17 +47,13 @@ func (p *Provider) ResolveDefinition(ctx provider.DefinitionContext) {
 			return
 		}
 
-		viewFile := laravel.ViewFromName(name)
-
-		ctx.Logger.Debug(viewFile)
-
-		fullPath, found := p.fs.find(p.rootPath, viewFile.Filename())
+		fullPath, found := p.finder.Find(name)
 
 		ctx.Logger.Debugf("%s %v", fullPath, found)
 
 		if found {
 			ctx.Publish(protocol.Location{
-				URI: protocol.DocumentURI(path.Join(p.rootPath, fullPath)),
+				URI: protocol.DocumentURI(fullPath),
 			})
 		}
 	}
@@ -66,19 +63,18 @@ func (p *Provider) ResolveCompletion(ctx provider.CompletionContext) {
 	node := queries.ViewNames(ctx.File).At(ctx.Position)
 
 	if node != nil {
-		text := php.GetStringContent(node, ctx.File.Src)
-
-		results, err := p.fs.search(p.rootPath, text)
-		if err != nil {
-			ctx.Logger.WithError(err).Error("failed to search view files")
-			return
-		}
+		input := php.GetStringContent(node, ctx.File.Src)
 
 		kind := protocol.CompletionItemKindFile
-		for _, result := range results {
+		for _, result := range p.finder.Search(input) {
+			rel, err := filepath.Rel(p.rootPath, result.Path())
+			if err != nil {
+				return
+			}
+
 			ctx.Publish(protocol.CompletionItem{
 				Label:  result.Name(),
-				Detail: result.Path(),
+				Detail: rel,
 				Kind:   &kind,
 			})
 		}
@@ -91,7 +87,7 @@ func (p *Provider) Diagnostic(ctx provider.DiagnosticContext) {
 		name := php.GetStringContent(&capture.Node, ctx.File.Src)
 
 		// Report diagnostic if view does not exist.
-		if !p.fs.exists(p.rootPath, name) {
+		if _, found := p.finder.Find(name); !found {
 			ctx.Publish(provider.Diagnostic{
 				Range:    capture.Node.Range(),
 				Severity: protocol.DiagnosticSeverityError,
@@ -110,9 +106,14 @@ func (p *Provider) Hover(ctx provider.HoverContext) {
 			return
 		}
 
-		if view, found := p.fs.findView(p.rootPath, name); found {
+		if viewPath, found := p.finder.Find(name); found {
+			rel, err := filepath.Rel(p.rootPath, viewPath)
+			if err != nil {
+				return
+			}
+
 			ctx.Publish(provider.Hover{
-				Content: view.Path(),
+				Content: rel,
 			})
 		}
 	}
@@ -127,28 +128,27 @@ func (p *Provider) ResolveCodeAction(ctx provider.CodeActionContext) {
 			return
 		}
 
-		viewFile := laravel.ViewFromName(name)
+		ctx.Logger.Debug(name)
 
-		if _, found := p.fs.findView(p.rootPath, viewFile.Name()); !found {
-			kind := protocol.CodeActionRefactor
-			fullPath := path.Join(p.rootPath, p.fs.paths[0], viewFile.Filename())
-			ctx.Publish(protocol.CodeAction{
-				Title: "Create view",
-				Kind:  &kind,
-				Edit: &protocol.WorkspaceEdit{
-					DocumentChanges: []protocol.DocumentChangeOperation{
-						protocol.CreateFile{
-							URI: protocol.DocumentURI("file://" + fullPath),
-							ResourceOperation: protocol.ResourceOperation{
-								Kind: "create",
-							},
-							Options: &protocol.CreateFileOptions{
-								IgnoreIfExists: true,
+		if _, found := p.finder.Find(name); !found {
+			for _, filename := range p.finder.PossibleFiles(name) {
+				filename, _ = filepath.Rel(p.rootPath, filename)
+				kind := protocol.CodeActionRefactor
+				ctx.Publish(protocol.CodeAction{
+					Title: fmt.Sprintf("Create view (%s)", filename),
+					Kind:  &kind,
+					Edit: &protocol.WorkspaceEdit{
+						DocumentChanges: []protocol.DocumentChangeOperation{
+							protocol.CreateFile{
+								URI: protocol.DocumentURI("file://" + filename),
+								ResourceOperation: protocol.ResourceOperation{
+									Kind: "create",
+								},
 							},
 						},
 					},
-				},
-			})
+				})
+			}
 		}
 	}
 }
