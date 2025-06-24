c049f8741c42d0b34deba3485273567d61cb64c6
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-06-24 18:54:50 +0200

    runtime/script.go: in CallScript() rename call variable to proc.

--- runtime/script.go
+++ runtime/script.go
@@ -2,8 +2,8 @@ package runtime
 
 import "encoding/json"
 
-func CallScript[T any](call Process, rootPath string, code []byte, out T) (T, error) {
-	output, err := call.Exec(rootPath, code)
+func CallScript[T any](proc Process, rootPath string, code []byte, out T) (T, error) {
+	output, err := proc.Exec(rootPath, code)
 	if err == nil {
 		err = json.NewDecoder(output).Decode(&out)
 	}
