6b1c5dcb0ee9a9b9dcca9b55443d2557f8b9b282
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2025-06-28 11:13:07 +0200

    cmd: make sure LogFile is open during the last call to the logger

--- cmd/laravel-ls/cmd/root.go
+++ cmd/laravel-ls/cmd/root.go
@@ -26,7 +26,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-var basePath string
+var (
+	basePath string
+	logFile  *os.File
+)
 
 // Expand "~" to user's home directory.
 func expandHome(path string) string {
@@ -65,11 +68,10 @@ func run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	logFile, err := openLogFile(cfg.Log.Filename)
+	logFile, err = openLogFile(cfg.Log.Filename)
 	if err != nil {
 		return err
 	}
-	defer logFile.Close()
 
 	log.SetOutput(logFile)
 	log.SetLevel(cfg.Log.Level)
@@ -109,7 +111,7 @@ func bindFlagsToConfig(flags *pflag.FlagSet) {
 	must(viper.BindPFlag("log.level", flags.Lookup("log-level")))
 }
 
-func Run() error {
+func Run() {
 	cmd := cobra.Command{
 		Use:     program.Name,
 		Short:   "Language server for Laravel",
@@ -124,5 +126,11 @@ func Run() error {
 
 	bindFlagsToConfig(cmd.PersistentFlags())
 
-	return cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		log.WithError(err).Error("Error")
+	}
+
+	if logFile != nil {
+		logFile.Close()
+	}
 }
--- cmd/laravel-ls/main.go
+++ cmd/laravel-ls/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/laravel-ls/laravel-ls/cmd/laravel-ls/cmd"
 	"github.com/laravel-ls/laravel-ls/program"
-	log "github.com/sirupsen/logrus"
 )
 
 var version string = ""
@@ -11,7 +10,5 @@ var version string = ""
 func main() {
 	program.VersionOverride = version
 
-	if err := cmd.Run(); err != nil {
-		log.WithError(err).Error("Error")
-	}
+	cmd.Run()
 }
