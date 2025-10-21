6f867e9d750bd468b1a17a3727b01f50e5eb3c77
Henrik Hautakoski <henrik@shufflingpixels.com>
2025-10-21 10:09:28 +0200

    file/type.go: in TypeByFilename() have all checks use the lookup table by having test functions, filetype pairs.

--- file/type.go
+++ file/type.go
@@ -14,22 +14,31 @@ const (
 	TypeEnv
 )
 
-// Find the filetype based on filename
+type checkFn func(string) bool
+
+func hasExtension(ext string) checkFn {
+	return func(filename string) bool {
+		return strings.HasSuffix(filename, ext)
+	}
+}
+
+func envFile(filename string) bool {
+	return strings.HasPrefix(filename, ".env")
+}
+
+// TypeByFilename finds the filetype based on filename
 func TypeByFilename(filename string) Type {
-	lookup := map[string]Type{
-		".blade.php": TypeBlade,
-		".php":       TypePHP,
+	lookup := map[Type]checkFn{
+		TypeBlade: hasExtension(".blade.php"),
+		TypePHP:   hasExtension(".php"),
+		TypeEnv:   envFile,
 	}
 
-	for ext, typ := range lookup {
-		if strings.HasSuffix(filename, ext) {
+	filename = path.Base(filename)
+	for typ, test := range lookup {
+		if ok := test(filename); ok {
 			return typ
 		}
 	}
-
-	if strings.HasPrefix(path.Base(filename), ".env") {
-		return TypeEnv
-	}
-
 	return TypeUnknown
 }
